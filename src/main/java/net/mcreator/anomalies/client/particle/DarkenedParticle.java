366d7b758c9b0df2dfe49f7365ee34facd52306a
Trowa8 <likepcandtv@gmail.com>
2025-08-12 19:09:22 +0300

    Add files via upload

---
+++ src/main/java/net/mcreator/anomalies/client/particle/DarkenedParticle.java
@@ -0,0 +1,63 @@
+package net.mcreator.anomalies.client.particle;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+@OnlyIn(Dist.CLIENT)
+public class DarkenedParticle extends TextureSheetParticle {
+	public static DarkenedParticleProvider provider(SpriteSet spriteSet) {
+		return new DarkenedParticleProvider(spriteSet);
+	}
+
+	public static class DarkenedParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public DarkenedParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new DarkenedParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+	private float angularVelocity;
+	private float angularAcceleration;
+
+	protected DarkenedParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(0.2f, 0.2f);
+		this.lifetime = (int) Math.max(1, 24 + (this.random.nextInt(10) - 5));
+		this.gravity = 0.8f;
+		this.hasPhysics = true;
+		this.xd = vx * 0;
+		this.yd = vy * 0;
+		this.zd = vz * 0;
+		this.angularVelocity = 0.04f;
+		this.angularAcceleration = 0.02f;
+		this.pickSprite(spriteSet);
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		this.oRoll = this.roll;
+		this.roll += this.angularVelocity;
+		this.angularVelocity += this.angularAcceleration;
+	}
+}
