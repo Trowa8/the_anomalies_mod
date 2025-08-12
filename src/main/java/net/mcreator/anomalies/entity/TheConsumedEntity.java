86735ff7bcc7b10ee9ab89a612749ef632e840bb
Trowa8 <likepcandtv@gmail.com>
2025-08-12 19:12:11 +0300

    Add files via upload

---
+++ src/main/java/net/mcreator/anomalies/entity/TheConsumedEntity.java
@@ -0,0 +1,136 @@
+package net.mcreator.anomalies.entity;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.network.PlayMessages;
+import net.minecraftforge.network.NetworkHooks;
+
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.AnimationState;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.Difficulty;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.Packet;
+
+import net.mcreator.anomalies.procedures.TheConsumedRunPlaybackConditionProcedure;
+import net.mcreator.anomalies.procedures.TheConsumedIdlePlaybackConditionProcedure;
+import net.mcreator.anomalies.init.AnomaliesModEntities;
+
+public class TheConsumedEntity extends Monster {
+	public final AnimationState animationState0 = new AnimationState();
+	public final AnimationState animationState2 = new AnimationState();
+
+	public TheConsumedEntity(PlayMessages.SpawnEntity packet, Level world) {
+		this(AnomaliesModEntities.THE_CONSUMED.get(), world);
+	}
+
+	public TheConsumedEntity(EntityType<TheConsumedEntity> type, Level world) {
+		super(type, world);
+		setMaxUpStep(1f);
+		xpReward = 8;
+		setNoAi(false);
+	}
+
+	@Override
+	public Packet<ClientGamePacketListener> getAddEntityPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
+	}
+
+	@Override
+	protected void registerGoals() {
+		super.registerGoals();
+		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, true) {
+			@Override
+			protected double getAttackReachSqr(LivingEntity entity) {
+				return this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth();
+			}
+		});
+		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, Player.class, false, true));
+		this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, (float) 30));
+		this.targetSelector.addGoal(4, new HurtByTargetGoal(this));
+		this.targetSelector.addGoal(5, new NearestAttackableTargetGoal(this, Animal.class, true, true));
+		this.goalSelector.addGoal(6, new RandomStrollGoal(this, 1));
+		this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(8, new FloatGoal(this));
+	}
+
+	@Override
+	public MobType getMobType() {
+		return MobType.ILLAGER;
+	}
+
+	@Override
+	public double getPassengersRidingOffset() {
+		return super.getPassengersRidingOffset() + 2.5;
+	}
+
+	@Override
+	public SoundEvent getHurtSound(DamageSource ds) {
+		return ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.hurt"));
+	}
+
+	@Override
+	public SoundEvent getDeathSound() {
+		return ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.death"));
+	}
+
+	@Override
+	public boolean hurt(DamageSource damagesource, float amount) {
+		if (damagesource.is(DamageTypes.IN_FIRE))
+			return false;
+		if (damagesource.is(DamageTypes.LIGHTNING_BOLT))
+			return false;
+		return super.hurt(damagesource, amount);
+	}
+
+	@Override
+	public boolean fireImmune() {
+		return true;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		if (this.level().isClientSide()) {
+			this.animationState0.animateWhen(TheConsumedIdlePlaybackConditionProcedure.execute(this), this.tickCount);
+			this.animationState2.animateWhen(TheConsumedRunPlaybackConditionProcedure.execute(this), this.tickCount);
+		}
+	}
+
+	public static void init() {
+		SpawnPlacements.register(AnomaliesModEntities.THE_CONSUMED.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)));
+	}
+
+	public static AttributeSupplier.Builder createAttributes() {
+		AttributeSupplier.Builder builder = Mob.createMobAttributes();
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.3);
+		builder = builder.add(Attributes.MAX_HEALTH, 40);
+		builder = builder.add(Attributes.ARMOR, 4);
+		builder = builder.add(Attributes.ATTACK_DAMAGE, 8);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 20);
+		builder = builder.add(Attributes.KNOCKBACK_RESISTANCE, 0.2);
+		builder = builder.add(Attributes.ATTACK_KNOCKBACK, 0.6);
+		return builder;
+	}
+}
